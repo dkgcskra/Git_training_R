81c1c3030484bc444ca5222ee452ab052acc8715
Dino Karagic <66651925+dkgcskra@users.noreply.github.com>
2025-09-19 13:35:49 +0200

    Update Test for git training.R

--- Test for git training.R
+++ Test for git training.R
@@ -1,4 +1,4 @@
 install.packages("tidyverse")
 install.packages("tidyAML")
-install.packages("tidybayes") #this is a test 1
-install.packages("tidymodels") #this is a test 1 hehe
+install.packages("tidybayes") #this is not a test 1
+install.packages("tidymodels") #this is a not test 1 hehe
